63c5a11cdf2dc0bc098542c3577919acec71f64d
mob35 <toonelicious@gmail.com>
2018-10-16 15:57:09 +0700

    เก็บข้อมูลที่ได้จาก Web API และเตรียม method ค้นหา user จาก email

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,15 +6,26 @@ import { RandomResult } from '../models/random-result';
   providedIn: 'root'
 })
 export class UserService {
-  
+
   selectedUser: any;
+  remoteData: RandomResult;
 
   constructor(
     private http: HttpClient
   ) { }
 
   async getUsers() {
-    let result = await this.http.get<RandomResult>('https://randomuser.me/api/?results=15').toPromise();
+    let result = await this.http.get<RandomResult>('https://randomuser.me/api/?results=10').toPromise();
+    this.remoteData = result;
     return result;
+  }
+  searchUserFromEmail(email) {
+    return this.remoteData.results.find((item) => {
+      return false;
+
+    }, (err) => {
+      return false;
+
+    });
   }
 }
